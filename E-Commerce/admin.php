565e99bee1911340793088a84f0f8c298b31f452
sagarbarbhaya <sagar.b.barbhaya@gmail.com>
2015-02-15 20:10:37 -0500

    E-Commerce

---
+++ E-Commerce/admin.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Sagar Barbhaya
+ * Date: 2/15/2015
+ * Time: 5:48 PM
+ */
---
+++ E-Commerce/lib_project1.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Sagar Barbhaya
+ * Date: 2/15/2015
+ * Time: 5:49 PM
+ */
+?>
+<html>
+<head>
+    <title>
+        Sagar's Ecommerce website
+    </title>
+    <link rel="stylesheet" type="text/css" href="Basepage.css">
+</head>
+<body>
+<div class="Header_bar">
+    <div class="Caption">
+        <label class="Fonts">Awesome products on Sale</label>
+    </div>
+    <span class="Cart">
+        <img src="cart.png" alt="Cart" height="30px" width="30px">
+        <label class="Fonts">Cart</label>
+    </span>
+    <div class="Search">
+    <label class="fonts">Search</label>
+    <input type="search" aria="search" role="search">
+    </div>
+</div>
+<div class="Template_bar">
+    <div class="Home">
+    <a href="lib_project1.php" class="Fonts">Home</a>
+    </div>
+    <div class="Catalog">
+        <a href="lib_project1.php" class="Fonts">Catalog</a>
+
+    </div>
+    <div class="Admin">
+        <a href="lib_project1.php" class="Fonts">Admin</a>
+
+    </div>
+    <div class="Contact-Us">
+        <a href="lib_project1.php" class="Fonts">Contact-Us</a>
+
+    </div>
+</div>
+<div class="Left_column">
+
+</div>
+<div class="Centre_Image">
+
+</div>
+<div class="Shopping_Matrix">
+
+</div>
+</body>
+</html>
